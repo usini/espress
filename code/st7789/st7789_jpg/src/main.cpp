096b1dc4cbaa716989c66d4203a9d9450f3953fc
Rémi Sarrailh <remi@usini.eu>
2022-10-05 17:15:58 +0200

    [ST7789] d1_mini added, change pong with JPG gallery example

---
+++ code/st7789/st7789_jpg/src/main.cpp
@@ -0,0 +1,57 @@
+#include <TFT_eSPI.h>
+#include <SPI.h>
+#include "FS.h"
+#include <LITTLEFS.h>
+#include <TJpg_Decoder.h>
+
+TFT_eSPI tft = TFT_eSPI(); // Ecran TFT
+
+bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t *bitmap)
+{
+  if (y >= tft.height())
+    return 0;
+  tft.pushImage(x, y, w, h, bitmap);
+  return 1;
+}
+
+void setup_tft()
+{
+  tft.begin();
+  if (!LittleFS.begin())
+  {
+    Serial.println("LittleFS is not properly setup");
+    return;
+  }
+  tft.setSwapBytes(true);
+  TJpgDec.setJpgScale(1);
+  TJpgDec.setCallback(tft_output);
+}
+
+void display_image(const char *filename)
+{
+  uint16_t w = 0, h = 0;
+  TJpgDec.drawFsJpg(0, 0, filename, LittleFS);
+}
+
+void setup()
+{
+  setup_tft();
+}
+
+void loop()
+{
+  display_image("ia.jpg");
+  delay(3000);
+  display_image("ia2.jpg");
+  delay(3000);
+  display_image("ia3.jpg");
+  delay(3000);
+  display_image("ia4.jpg");
+  delay(3000);
+  display_image("ia5.jpg");
+  delay(3000);
+  display_image("ia6.jpg");
+  delay(3000);
+  display_image("ia7.jpg");
+  delay(3000);
+}
