b858deb99dcb2912034cbd84df351eeec06b96e6
Rémi Sarrailh <remi@usini.eu>
2022-10-05 17:13:11 +0200

    [LED] d1_mini added

--- code/led/led_test/src/main.cpp
+++ code/led/led_test/src/main.cpp
@@ -8,15 +8,14 @@
   const int LED_PIN = 10;
 #endif
 
-#ifdef LOLIN32
-  const int LED_PIN = 22;
+#ifdef WEMOS_D1_MINI
+  const int LED_PIN = D4;
 #endif
-
 
 void setup() {
   Serial.begin(115200);
   Serial.println("LED Blink - https://usini.eu/espress/");
-  pinMode(LED_PIN, OUTPUT); // LOLIN D32
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
@@ -27,3 +26,4 @@ void loop() {
   digitalWrite(LED_PIN, LOW);
   delay(1000);
 }
+
