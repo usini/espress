c8af60321b3a30da270334b22a4043e7deb94634
Rémi Sarrailh <remi@usini.eu>
2022-10-16 13:29:56 +0200

    2.3.3 Move navbar to navbar.html

--- js/default.js
+++ js/default.js
@@ -1,4 +1,4 @@
-
+includeHTML();
 // Load translation
 var script_i18n = document.createElement('script');
 script_i18n.src = "js/i18n.js"
@@ -16,7 +16,7 @@ if (localStorage.getItem("data-theme") == null) {
     localStorage.setItem("data-theme", "dark")
 }
 
-change_theme_icon();
+
 function change_theme() {
     theme = localStorage.getItem("data-theme")
     if (theme == "dark") {
@@ -37,4 +37,33 @@ function change_theme_icon() {
         document.getElementById("change-theme").innerHTML = "🌑";
         document.getElementById("logo").style = "color:black;"  
     }
+}
+
+function includeHTML(){
+        var z, i, elmnt, file, xhttp;
+        /* Loop through a collection of all HTML elements: */
+        z = document.getElementsByTagName("*");
+        for (i = 0; i < z.length; i++) {
+          elmnt = z[i];
+          /*search for elements with a certain atrribute:*/
+          file = elmnt.getAttribute("w3-include-html");
+          if (file) {
+            /* Make an HTTP request using the attribute value as the file name: */
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+              if (this.readyState == 4) {
+                if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                /* Remove the attribute, and call this function once more: */
+                elmnt.removeAttribute("w3-include-html");
+                includeHTML();
+                change_theme_icon();
+              }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /* Exit the function: */
+            return;
+          }
+        }
 }
